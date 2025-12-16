deed861479a59c96441b4f735be6877d41785215
اسمك هنا < >
2025-12-16 07:52:30 +0300

    ..

--- app/page.js
+++ app/page.js
@@ -1,6 +1,10 @@
 import Quotes from "./data/Quotes.jsx";
+
+export const metadata = {
+  title: "Quotes",
+  description: "Quotes",
+};
 export default function Home() {
-  
   return (
     <div className="flex min-h-screen items-center justify-center w-screen bg-linear-to-r from-cyan-400 via-blue-300 via-0%0 to-green-300" dir="rtl">
       <Quotes />
